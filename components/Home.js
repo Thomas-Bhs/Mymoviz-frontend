4abc57bd43a22d10ec02edf1bbd4963d55d1e696
Thomas <bourchisthomas@gmail.com>
2025-11-12 15:10:41 +0100

    second commit front

--- components/Home.js
+++ components/Home.js
@@ -49,7 +49,7 @@ function Home() {
   const [moviesData, setMoviesData] = useState([]);
  
   useEffect(() => {
-    fetch('http://localhost:3000/movies')
+    fetch('https://mymoviz-backend-chi-six.vercel.app/movies')
       .then(response => response.json())
       .then(data => {
         setMoviesData(data.movies);
